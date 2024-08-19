7e6db5bd88a56ca31d28b7aaa4d62c37c8114fc9
minturtle <minseok0425@kakao.com>
2024-08-19 23:47:44 +0900

    feat: Initial Commit

---
+++ src/main/kotlin/com/minturtle/cs/CsStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.minturtle.cs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CsStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<CsStudyApplication>(*args)
+}
