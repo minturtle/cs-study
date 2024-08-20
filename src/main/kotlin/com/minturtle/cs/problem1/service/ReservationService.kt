22fc617e5e9a52986030b4b12c82845c7a3e62ef
minturtle <minseok0425@kakao.com>
2024-08-21 01:55:34 +0900

    feat: 문제 해결

--- src/main/kotlin/com/minturtle/cs/problem1/service/ReservationService.kt
+++ src/main/kotlin/com/minturtle/cs/problem1/service/ReservationService.kt
@@ -1,6 +1,7 @@
 package com.minturtle.cs.problem1.service
 
 import com.minturtle.cs.problem1.entity.Reservation
+import com.minturtle.cs.problem1.exception.MyDataIntegrationException
 import com.minturtle.cs.problem1.repository.ReservationRepository
 import org.springframework.stereotype.Service
 import java.lang.RuntimeException
@@ -9,22 +10,52 @@ import java.util.concurrent.atomic.AtomicInteger
 
 @Service
 class ReservationService(
-    private val reservationRepository: ReservationRepository
+    private val reservationRepository: ReservationRepository,
+    private val cacheManager: ReservationCacheService
 ) {
 
-    private val idSeq = AtomicInteger(0)
-
     fun save(dateId : Long, seatId: Long){
-        val newEntity = Reservation(idSeq.incrementAndGet().toString(), dateId, seatId)
+        val id = createReservationId(dateId, seatId)
 
-        reservationRepository.save(newEntity)
+        if(findById(id) != null){
+            throw RuntimeException()
+        }
+
+        val newEntity = Reservation(
+            id,
+            dateId,
+            seatId
+        )
+
+        try{
+            reservationRepository.save(newEntity)
+        }catch (e: MyDataIntegrationException){
+            throw RuntimeException()
+        }
+
     }
 
-    fun findById(id : String): Reservation {
-        return reservationRepository.findById(id) ?: throw RuntimeException()
+    fun findById(id : String): Reservation? {
+        var tmp = cacheManager.get(id)
+
+        if(tmp != null){
+            return tmp
+        }
+
+        tmp = reservationRepository.findById(id)
+        if(tmp != null){
+            cacheManager.add(id, tmp)
+            return tmp
+        }
+
+        return null
     }
 
     fun findAll(): Collection<Reservation> {
         return reservationRepository.findAll()
+    }
+
+    private fun createReservationId(dateId : Long, seatId: Long): String{
+        return "RESERVATION_${dateId}_${seatId}"
     }
 }
