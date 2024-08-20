1100f313369d2f41b04f34ffe35f013709aebaa5
minturtle <minseok0425@kakao.com>
2024-08-21 01:56:33 +0900

    fix: 문제 1번의 Repository를 실제 DB에 가깝게 구현

--- src/main/kotlin/com/minturtle/cs/problem1/repository/ReservationRepository.kt
+++ src/main/kotlin/com/minturtle/cs/problem1/repository/ReservationRepository.kt
@@ -3,15 +3,17 @@ package com.minturtle.cs.problem1.repository
 import com.minturtle.cs.problem1.entity.Reservation
 import com.minturtle.cs.problem1.exception.MyDataIntegrationException
 import org.springframework.stereotype.Repository
+import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.atomic.AtomicInteger
 
 
 @Repository
 class ReservationRepository {
 
-    private val reservationDao : MutableMap<String, Reservation> = HashMap()
+    private val reservationDao : MutableMap<String, Reservation> = ConcurrentHashMap()
 
     fun save(entity: Reservation){
+        Thread.sleep(10) // 실제 DB의 Blocking
         val previousValue = reservationDao.putIfAbsent(entity.id, entity)
         if (previousValue != null) {
             throw MyDataIntegrationException()
@@ -19,10 +21,12 @@ class ReservationRepository {
     }
 
     fun findById(id: String): Reservation?{
+        Thread.sleep(10) // 실제 DB의 Blocking
         return reservationDao[id]
     }
 
     fun findAll(): Collection<Reservation>{
+        Thread.sleep(100) // 실제 DB의 Blocking
         return reservationDao.values
     }
 
